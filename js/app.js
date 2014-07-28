41736179727b18a4c861bf9cdadd66f6a8016dba
zconn08 <zconn@unknowna8bbcf21d658.gateway.pace.com>
2014-07-27 19:57:58 -0700

    JS work 1

--- js/app.js
+++ js/app.js
@@ -1,16 +1,75 @@
+var correctAnswer;
+var guess;
+var counter;
+
+var randomNumberGenerator = function(){
+	return Math.floor((Math.random()*100+1));
+};
+
+var newGame = function(){
+	correctAnswer = randomNumberGenerator();
+};
 
 $(document).ready(function(){
 	
+	var retrieveGuess = function(){
+		guess = $("#userGuess").val();
+	};
+
+	var provideFeedback = function(){
+		if (guess === correctAnswer) {
+			console.log("Correct");
+		}
+		else if(guess - correctAnswer < 10){
+			console.log("Very Hot");
+		}
+		else if(guess - correctAnswer < 20){
+			console.log("Hot");
+		}
+		else if(guess - correctAnswer < 30){
+			console.log("Warm");
+		}
+		else if(guess - correctAnswer < 40){
+			console.log("Cold");
+		}
+		else{
+			console.log("Ice Cold");
+		}
+	};
+
+	var addGuess = function(){
+		$(".guessList").append("<li>" + guess + "<li>");
+	};
+
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+		$(".overlay").fadeIn(1000);
+	});
 
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+	$(".overlay").fadeOut(1000);
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	//Retrieve guess and provide feedback
+	$("#guessButton").click(function(){
+		retrieveGuess();
+		provideFeedback();
+	});
+	//Start game upon page load
+	newGame();
+	//Start game upon clicking on new game
+	$(".new").click(function(){
+		newGame();
+	});
+
+	//Append guessList with guess #
+	$("#guessButton").click(function(){
+		retrieveGuess();
+		addGuess();
+	});
+
+
 
 });
 
